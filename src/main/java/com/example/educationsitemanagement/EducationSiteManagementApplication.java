3dcff0c9eb29617d7f7a93ee0a1c71f1ec009cd0
rsakin <ramazan.sakin@solve.care>
2022-07-08 13:29:01 +0300

    init project

---
+++ src/main/java/com/example/educationsitemanagement/EducationSiteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.educationsitemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationSiteManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationSiteManagementApplication.class, args);
+    }
+
+}
