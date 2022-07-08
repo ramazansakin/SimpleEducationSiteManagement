bddfa6cfda60872efbeacf2a86ef1310801d39b6
rsakin <ramazan.sakin@solve.care>
2022-07-08 13:30:59 +0300

    Init update with basic files

---
+++ src/test/java/com/example/educationsitemanagement/EducationSiteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educationsitemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationSiteManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
