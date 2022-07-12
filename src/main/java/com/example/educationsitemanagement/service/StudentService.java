9b984b9e533fc50043efeab610229e5bb0aba840
rsakin <ramazansakin63@gmail.com>
2022-07-12 16:34:30 +0300

    service definitions

---
+++ src/main/java/com/example/educationsitemanagement/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.example.educationsitemanagement.service;
+
+import com.example.educationsitemanagement.repo.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    // CRUD Operations
+
+
+
+}
