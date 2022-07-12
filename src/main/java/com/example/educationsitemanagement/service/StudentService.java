b996cac826d51fc66ea5f7bce2d5d47396d28182
rsakin <ramazansakin63@gmail.com>
2022-07-12 18:40:54 +0300

    some training on student service playground

--- src/main/java/com/example/educationsitemanagement/service/StudentService.java
+++ src/main/java/com/example/educationsitemanagement/service/StudentService.java
@@ -1,8 +1,11 @@
 package com.example.educationsitemanagement.service;
 
+import com.example.educationsitemanagement.model.Student;
 import com.example.educationsitemanagement.repo.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -11,7 +14,41 @@ public class StudentService {
     private StudentRepository studentRepository;
 
     // CRUD Operations
+    // Business Logic implementations
+//    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.getAll();
+    }
 
+    //    @Override
+    public Student getStudent(Integer id) {
+        return studentRepository.getById(id);
+    }
+
+    //    @Override
+    public void addStudent(Student airport) {
+        studentRepository.create(airport);
+    }
+
+    //    @Override
+    public Student updateStudent(Integer id, Student student) throws CloneNotSupportedException {
+        getStudent(id);
+        try {
+            return studentRepository.update(student, id);
+        } catch (CloneNotSupportedException e) {
+            // Need to handle exception or throw it to upper
+//            throw new CloneNotSupportedException();
+            return null;
+        }
+    }
+
+    //    @Override
+    public boolean deleteStudent(Integer id) {
+        // check if its already there or not !
+        getStudent(id);
+        studentRepository.delete(id);
+        return true;
+    }
 
 
 }
